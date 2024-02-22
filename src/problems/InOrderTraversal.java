f5c3a523364f1ae9a7b9a4942afdf233a50c9544
Imran-Haider-47 <imranhaider0247@gmail.com>
2024-02-22 10:53:24 +0200

    Town Judge

--- src/problems/InOrderTraversal.java
+++ src/problems/InOrderTraversal.java
@@ -1,43 +1,44 @@
-
 package problems;
 
-import java.util.*;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
-class PreOrderTraversal {
-
+public class InOrderTraversal {
 
     public static void main(String[] args) {
         List<Integer> list = List.of(1,2,3,4,5);
-        PreOrderTraversal pathsum = new PreOrderTraversal();
-        TreeNode treeNode = pathsum.buildTree(list);
-
-        List<Integer> result = new ArrayList<>();
-        PreOrderTraversal.preorderTraversal(treeNode, result);
-
-        System.out.println(result);
-
-
+        InOrderTraversal inOrder = new InOrderTraversal();
+        TreeNode root= inOrder.buildTree(list);
+        inOrderTraversal(root, list);
     }
-    public static class TreeNode {
+    public static class TreeNode{
         int val;
         TreeNode left;
         TreeNode right;
-        TreeNode() {}
-        TreeNode(int val) { this.val = val; }
-        TreeNode(int val, TreeNode left, TreeNode right) {
+        TreeNode(){}
+        TreeNode(int val)
+        {
+            this.val = val;
+        }
+        TreeNode(int val, TreeNode left, TreeNode right){
             this.val = val;
             this.left = left;
-            this.right = right;
+            this. right = right;
         }
-    }
+
+
+    }// TreeNode class ends here
     public TreeNode buildTree(List<Integer> values){
-        int n = values.size();
         Queue<TreeNode> queue = new LinkedList<>();
+        int n = values.size();
+
         TreeNode root = new TreeNode(values.get(0));
         queue.add(root);
+
         int i=1;
         while(i<n && !queue.isEmpty()){
-            TreeNode currentNode  = queue.poll();
+            TreeNode currentNode = queue.poll();
             currentNode.left = new TreeNode(values.get(i));
             queue.add(currentNode.left);
             i=i+1;
@@ -46,22 +47,19 @@ class PreOrderTraversal {
             }
             currentNode.right = new TreeNode(values.get(i));
             queue.add(currentNode.right);
-
             i=i+1;
-
-        }
-
+          }
         return root;
+
+
     }
-
-
-    public static void preorderTraversal(TreeNode root, List<Integer> values) {
-
-        if(root!=null) {
-
+    public static void inOrderTraversal(TreeNode root,List<Integer> values)
+    {
+        if(root!=null){
+            inOrderTraversal(root,values);
             values.add(root.val);
-            preorderTraversal(root.left, values);
-            preorderTraversal(root.right, values);
+            inOrderTraversal(root, values);
         }
     }
+
 }

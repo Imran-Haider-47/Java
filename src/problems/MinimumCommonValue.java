c388118c4db743c8815e0d3fc822f18bf95561ae
Imran-Haider-47 <imranhaider0247@gmail.com>
2024-03-21 15:40:37 +0300

    Minimum Common Value

--- src/problems/MinimumCommonValue.java
+++ src/problems/MinimumCommonValue.java
@@ -1,10 +1,19 @@
 package problems;
 
+import java.util.HashSet;
+
 public class MinimumCommonValue {
     int[] nums1 = {1, 2, 3};
     int[] nums2 = {2, 4};
-    public int solution(){
-
-        return 1;
+    public int solution(){HashSet<Integer> hashSet = new HashSet<>();
+        for(int i=0; i< nums1.length;i++){
+            hashSet.add(nums1[i]);
+        }
+        for(int i=0; i<nums2.length;i++){
+            if(hashSet.contains(nums2[i])){
+                return nums2[i];
+            }
+        }
+        return -1;
     }
 }
